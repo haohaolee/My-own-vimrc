dc7c7b63ebe69442fcdae3164220d1a4a1b2763f
haohaolee <hayatelee@gmail.com>
2010-10-12 14:39:44 +0800

    Fix an error intro For README

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ echo '
 fun! MySys()
    return "$1"
 endfun
-set runtimepath=~/.vim_runtime,~/.vim_runtime/after,\$VIMRUNTIME
 source ~/.vim_runtime/vimrc
 helptags ~/.vim_runtime/doc' > ~/.vimrc
 echo "Installed amix's vim configuration successfully! Enjoy :)"
